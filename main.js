d6d001af29dc8f51ee6f070a76d6579d74d0c0e3
Shane <shanemcneil19@gmail.com>
2020-05-02 18:41:09 -0700

    create function to check if 2 arrays create same bst

--- main.js
+++ main.js
@@ -103,19 +103,103 @@ function isBST(tree) {
     return false;
 }
 
-console.log(isBST(buildNumbers()));
+// console.log(isBST(buildNumbers()));
 
 function nthLargest(tree, n, count = 0) {
-    if (tree == null || count > n) {
+    if (tree == null || count >= n) {
         return;
     }
     nthLargest(tree.right, n, count);
     count++;
-    console.log(count);
     if (count == n) {
-        return tree.key;
+        console.log(tree.key) ;
     }
     nthLargest(tree.left, n, count);
 }
+// nthLargest(buildNumbers(), 3);
 
-console.log(nthLargest(buildNumbers(), 3));
+function isBalanced(tree) {
+    function minDepth(tree, count = 0) {
+        if (tree === null) {
+            return 0;
+        }
+        if (!tree.right && !tree.left) {
+            return count + 1;
+        } else if (tree.right && !tree.left) {
+            return height(tree.right, count + 1)
+        } else if (!tree.right && tree.left) {
+            return height(tree.left, count + 1)
+        } else if (tree.right && tree.left) {
+            return height(tree.right, count + 1) > height(tree.left, count + 1)
+            ? height(tree.left, count + 1)
+            : height(tree.right, count + 1);
+        }
+    }
+    function maxDepth(tree, count = 0) {
+        if (tree === null) {
+            return 0;
+        }
+        if (!tree.right && !tree.left) {
+            return count + 1;
+        } else if (tree.right && !tree.left) {
+            return height(tree.right, count + 1)
+        } else if (!tree.right && tree.left) {
+            return height(tree.left, count + 1)
+        } else if (tree.right && tree.left) {
+            return height(tree.right, count + 1) > height(tree.left, count + 1)
+            ? height(tree.right, count + 1)
+            : height(tree.left, count + 1);
+        }
+    }
+
+    // console.log('min', minDepth(tree), 'max', maxDepth(tree));
+    if (maxDepth(tree) - minDepth(tree) <= 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// console.log(isBalanced(buildLetters()));
+
+function mathHeight(tree) {
+    if (!tree) {
+        return 0;
+    }
+    return 1 + Math.max(mathHeight(tree.left), mathHeight(tree.right));
+}
+
+// console.log(mathHeight(buildNumbers()));
+
+function createSameBST(arr1, arr2) {
+    if (arr1.length !== arr2.length || arr1[0] !== arr2[0]) {
+        return false;
+    }
+    if (arr1.length === 0) {
+        return true;
+    }
+
+    let lessThanArr1 = [];
+    let greaterThanArr1 = [];
+    let lessThanArr2 = [];
+    let greaterThanArr2 = [];
+
+    for (let i = 1; i < arr1.length; i++) {
+        if (arr1[i] < arr1[0]) {
+            lessThanArr1.push(arr1[i]);
+        } else {
+            greaterThanArr1.push(arr1[i]);
+        }
+    }
+
+    for (let i = 1; i < arr2.length; i++) {
+        if (arr2[i] < arr2[0]) {
+            lessThanArr2.push(arr2[i]);
+        } else {
+            greaterThanArr2.push(arr2[i]);
+        }
+    }
+    return createSameBST(lessThanArr1, lessThanArr2) && createSameBST(greaterThanArr1, greaterThanArr2);
+}
+
+console.log(createSameBST([3, 5, 4, 6, 1, 0, 2], [3, 1, 5, 2, 4, 6, 0]));
